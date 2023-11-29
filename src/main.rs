1537b0eddeaabdecc5a3e0a8e72a3607ffbc1211
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-29 20:03:36 +0530

    resolved bug: trying to read when connection is dropped

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-#![allow(dead_code)]
-
 use std::net::IpAddr;
 use std::net::SocketAddr;
 use std::net::TcpListener;
 use std::net::{Shutdown, TcpStream};
 use std::result;
+use std::str;
 use std::thread;
 use std::fmt;
 use std::io::{Write, Read};
@@ -18,6 +17,8 @@ type Result<T> = result::Result<(), T>;
 
 const SAFE_MODE: bool = true;
 const BAN_LIMIT: Duration = Duration::from_secs(10*60);
+const MESSAGE_RATE: Duration = Duration::from_secs(1);
+const STRIKE_LIMIT: i32 = 10;
 
 struct Sensitive<T> (T);
 
@@ -33,23 +34,31 @@ impl<T: Display> Display for Sensitive<T> {
 }
 
 fn client(stream: Arc<TcpStream>, messages: Sender<Message>) -> Result<()>{
+    let author_addr = stream.peer_addr().map_err(|err| {
+        eprintln!("ERROR: could not get peer address {err}");
+    })?;
     messages.send(Message::ClientConnected{author: stream.clone()}).map_err(|err| {
         eprintln!("ERROR: could not send message to the server thread: {err}");
     })?;
     let mut buffer = Vec::new();
     buffer.resize(64, 0);
     loop {
-        let author_addr = stream.peer_addr().expect("TODO: cache the peer");
-
         let n = stream.as_ref().read(&mut buffer).map_err(|err| {
             eprintln!("ERROR: could not read message from client: {err}");
             let _ =  messages.send(Message::ClientDisconnected{author_addr});
         })?;
-
-        let _ = messages.send(Message::NewMessage{bytes: buffer[0..n].to_vec(), author_addr}).map_err(|err| {
-            eprintln!("ERROR: could not read message from client: {err}");
-        });
+        if n > 0 {
+            let _ = messages.send(Message::NewMessage{bytes: buffer[0..n].to_vec(), author_addr}).map_err(|err| {
+                eprintln!("ERROR: could not read message from client: {err}");
+            });
+        } else {
+            let _ = messages.send(Message::ClientDisconnected{author_addr}).map_err(|err| {
+                eprintln!("ERROR: could not send message to the server thread: {err}");
+            });
+            break;
+        }
     }
+    Ok(())
 }
 
 enum Message {
@@ -92,15 +101,17 @@ fn server(messages: Receiver<Message>) -> Result<()> {
                     }
 
                 });
-                
+
                 if let Some(banned_at) = banned_at  {
                     banned_clients.insert(author_addr.ip(), banned_at);
                     let mut author = author.as_ref();
-
                     let diff = now.duration_since(banned_at).expect("TODO: don't crash if the clock went backwards");
+                    let secs = (BAN_LIMIT - diff).as_secs_f32();
+                    println!("INFO: Client {author_addr} tried to connect but that client is bannedfor {secs} secs left");
                     let _ = writeln!(author, "you are banned!: {secs} secs left", secs = diff.as_secs_f32());
                     let _ = author.shutdown(Shutdown::Both);
                 }else {
+                    eprintln!("INFO: Client {author_addr} connected");
                     clients.insert(author_addr.clone(), Client {
                         conn: author.clone(),
                         last_message: now, 
@@ -109,24 +120,48 @@ fn server(messages: Receiver<Message>) -> Result<()> {
                 }
             },
             Message::ClientDisconnected{author_addr} => {
+                eprintln!("INFO: Client {author_addr} disconnected");
                 clients.remove(&author_addr);
             },
             Message::NewMessage{author_addr, bytes }=> {
-                let author = clients.get(&author_addr);
-                // let now = SystemTime::now();
-                if let Some(author) = clients.get(&author_addr) {
-                }
-                for (addr, client) in clients.iter() {
-                    if *addr != author_addr {
-                        let _ = client.conn.as_ref().write(&bytes);
+
+                if let Some(author) = clients.get_mut(&author_addr) {
+                    let now = SystemTime::now();
+                    let diff = now.duration_since(author.last_message).expect("TODO: don't crash is the clock went backwards");
+                    if diff >= MESSAGE_RATE {
+                        if let Ok(text) = str::from_utf8(&bytes) {
+                            eprintln!("INFO: Client {author_addr} send message {text}");
+                            for (addr, client) in clients.iter() {
+                                if *addr != author_addr {
+                                    let _ = client.conn.as_ref().write(&bytes);
+                                }
+                            }        
+                        }else {
+                            author.strike_count += 1;
+                            if author.strike_count >= STRIKE_LIMIT {
+                                eprintln!("INFO: Client {author_addr} is banned");
+                                banned_clients.insert(author_addr.ip().clone(), now);
+                                let _ = writeln!(author.conn.as_ref(), "You are banned!");
+                                let _ = author.conn.shutdown(Shutdown::Both);
+                            }
+                        }
+                    }else {
+                        author.strike_count += 1;
+                        if author.strike_count >= STRIKE_LIMIT {
+                            eprintln!("INFO: Client {author_addr} is banned");
+                            banned_clients.insert(author_addr.ip().clone(), now);
+                            let _ = writeln!(author.conn.as_ref(), "You are banned!");
+                            let _ = author.conn.shutdown(Shutdown::Both);
+                        }
                     }
                 }
+
             },
         }
     }
 }
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let address = "0.0.0.0:8000";
     let listener = TcpListener::bind(address).map_err(|err| {
         eprintln!("ERROR: could not bind {}: {}",Sensitive(address), Sensitive(err));
