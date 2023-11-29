a94ed984e60bc1649d494c34d9870f83e8b0993d
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-29 14:18:20 +0530

    chat working

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::net::TcpStream;
 use std::result;
 use std::thread;
 use std::fmt;
-use std::ops::Deref;
-use std::io::Read;
+use std::io::{Write, Read};
 use std::fmt::Display;
 use std::sync::mpsc::{Receiver, Sender, channel};
 use std::sync::Arc;
+use std::collections::HashMap;
 
 type Result<T> = result::Result<(), T>;
 
@@ -29,31 +29,66 @@ impl<T: Display> Display for Sensitive<T> {
 }
 
 fn client(stream: Arc<TcpStream>, messages: Sender<Message>) -> Result<()>{
-    messages.send(Message::ClientConnected(stream.clone())).map_err(|err| {
+    messages.send(Message::ClientConnected{author: stream.clone()}).map_err(|err| {
         eprintln!("ERROR: could not send message to the server thread: {err}");
     })?;
     let mut buffer = Vec::new();
     buffer.resize(64, 0);
     loop {
-        let n = stream.deref().read(&mut buffer).map_err(|err| {
+        let n = stream.as_ref().read(&mut buffer).map_err(|err| {
             eprintln!("ERROR: could not read message from client: {err}");
-            let _ =  messages.send(Message::ClientDisconnected);
+            let _ =  messages.send(Message::ClientDisconnected{ author: stream.clone()});
         })?;
 
-        let _ = messages.send(Message::NewMessage(buffer[0..n].to_vec())).map_err(|err| {
+        let _ = messages.send(Message::NewMessage{bytes: buffer[0..n].to_vec(), author: stream.clone()}).map_err(|err| {
             eprintln!("ERROR: could not read message from client: {err}");
         });
     }
 }
 
 enum Message {
-    ClientConnected(Arc<TcpStream>),
-    ClientDisconnected,
-    NewMessage(Vec<u8>),
+    ClientConnected{
+        author: Arc<TcpStream>,
+    },
+    ClientDisconnected{
+        author: Arc<TcpStream>,
+    },
+    NewMessage{
+        author: Arc<TcpStream>,
+        bytes: Vec<u8>,
+    }
 }
 
-fn server(_messages: Receiver<Message>) -> Result<()> {
-    todo!()
+struct Client {
+    conn: Arc<TcpStream>,
+}
+
+fn server(messages: Receiver<Message>) -> Result<()> {
+    let mut clients = HashMap::new();
+    loop {
+        let msg = messages.recv().expect("The server receiver is not hung up");
+        match msg {
+            Message::ClientConnected{author} => {
+                let addr = author.peer_addr().expect("TODO: cache the peer addr of the connection");
+                clients.insert(addr.clone(), Client {
+                    conn: author.clone(),
+                });
+            },
+            Message::ClientDisconnected{author} => {
+                let addr = author.peer_addr().expect("TODO: cache the peer addr of the connection");
+                clients.remove(&addr);
+            },
+            Message::NewMessage{author, bytes }=> {
+                let author_addr = author.peer_addr().expect("TODO: cache the peer addr of the connection");
+
+                for (addr, client) in clients.iter() {
+                    if *addr != author_addr {
+                        let _ = client.conn.as_ref().write(&bytes);
+                    }
+                }
+            },
+        }
+    }
 }
 
 fn main() -> Result<()>{
